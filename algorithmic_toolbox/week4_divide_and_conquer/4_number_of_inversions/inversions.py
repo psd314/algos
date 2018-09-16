02ae317e31f6ed781ec25d70b8ba4409c7671ffd
Philippe Dixon <phil.dixon@comparex.com>
2018-09-16 12:38:45 -0400

    semi-working inversions.py

--- algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,6 +1,26 @@
 # Uses python3
 import sys
 
+def merge_sort(a1, a2):
+    inversions = 0
+    a_prime = []
+    i, j = 0, 0
+    while i < len(a1) and j < len(a2):
+        b = a1[0]
+        c = a2[0]
+        if b <= c:
+            a_prime.append(b)
+            i += 1
+        else:
+            a_prime.append(c)
+            inversions += 1
+            j += 1
+    if i < len(a1) - 1:
+        a_prime += a1[i:]
+    if j < len(a2) - 1:
+        a_prime += a2[j:]
+
+    return inversions
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
     if right - left <= 1:
@@ -8,7 +28,7 @@ def get_number_of_inversions(a, b, left, right):
     ave = (left + right) // 2
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
+    number_of_inversions += merge_sort(a[left:ave], a[ave:right])
     return number_of_inversions
 
 if __name__ == '__main__':
