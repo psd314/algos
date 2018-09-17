a80e2129959935e9a2c5f622be638c5974005497
Philippe Dixon <pdixon555@hotmail.com>
2018-09-17 07:27:59 -0400

    progress on inversions

--- algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ algorithmic_toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,26 +1,32 @@
 # Uses python3
 import sys
+import random
 
 def merge_sort(a1, a2):
+    # print('a1:', a1, 'a2:', a2)
     inversions = 0
     a_prime = []
     i, j = 0, 0
     while i < len(a1) and j < len(a2):
-        b = a1[0]
-        c = a2[0]
+        b = a1[i]
+        c = a2[j]
         if b <= c:
             a_prime.append(b)
             i += 1
         else:
             a_prime.append(c)
-            inversions += 1
+            inversions += len(a1) - i
             j += 1
-    if i < len(a1) - 1:
+        # print('a prime_', a_prime)
+    if i < len(a1):
         a_prime += a1[i:]
-    if j < len(a2) - 1:
+    if j < len(a2):
+        # print('len a2', len(a2))
         a_prime += a2[j:]
-
+    # print('i:', i, 'j:', j)
+    # print('a prime:', a_prime, '\n')
     return inversions
+
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
     if right - left <= 1:
@@ -31,8 +37,35 @@ def get_number_of_inversions(a, b, left, right):
     number_of_inversions += merge_sort(a[left:ave], a[ave:right])
     return number_of_inversions
 
+def brute(a):
+    inversions = 0
+    for i in range(len(a)):
+        for j in range(i, len(a)):
+            if a[i] > a[j]:
+                inversions += 1
+    return inversions
+
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    # print('a:', a)
+    # print(get_number_of_inversions(a, b, 0, len(a)))
+    print(brute([4,1,5,2,5,2]))
+
+    # equal = True
+    # i = 0
+    # while equal and i < 10000:
+    #     a = [ random.randint(1,5) for i in range(6) ]
+    #     x = brute(a)
+    #     y = get_number_of_inversions(a, [], 0, len(a))
+
+    #     if x != y:
+    #         equal = False
+    #         print('i:', i)
+    #         print('a:', a)
+    #         print('brute:', x)
+    #         print('solution:', y)
+    #     i += 1
+
