c056bf65963b9c261a66fadfc51d33a69b1ecc62
Philippe Dixon <pdixon555@hotmail.com>
2018-09-24 06:54:27 -0400

    working on lottery problem

--- algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,10 +1,26 @@
 # Uses python3
 import sys
 
+def binary_search(val, points):
+    left = 0
+    right = len(points)
+    mid = len(points) // 2
+
+    return 0
+
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
     #write your code here
+    for i in range(len(starts)):
+        if starts[i] == ends[i]:
+            cnt = binary_search(starts[i], points)
+        else:
+            left = binary_search(starts[i], points)
+            right = binary_search(ends[i], points)
+            cnt[i] = right - left
     return cnt
+
+
 
 # def naive_count_segments(starts, ends, points):
 #     cnt = [0] * len(points)
@@ -22,6 +38,9 @@ if __name__ == '__main__':
     starts = data[2:2 * n + 2:2]
     ends   = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
+    points.sort()
+    print(points)
+    print(starts, ends)
     #use fast_count_segments
     # cnt = naive_count_segments(starts, ends, points)
     # for x in cnt:
@@ -30,3 +49,5 @@ if __name__ == '__main__':
 # test0 - 1 0 0
 # test1 - 0 0 1
 # test2 - 2 0
+
+# foreach segment - binary search endpoints in points, get indexes then subtract
