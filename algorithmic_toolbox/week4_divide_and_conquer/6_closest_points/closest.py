9a159e7d7b6c81ea1fc4081ba6a059d6ffdb5a7c
Philippe Dixon <phil.dixon@comparex.com>
2018-10-04 09:16:53 -0400

    starting closest_points

--- algorithmic_toolbox/week4_divide_and_conquer/6_closest_points/closest.py
+++ algorithmic_toolbox/week4_divide_and_conquer/6_closest_points/closest.py
@@ -2,9 +2,22 @@
 import sys
 import math
 
-def minimum_distance(x, y):
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+def minimum_distance(a):
     #write your code here
-    return 10 ** 18
+    print(x, y)
+    
+    if len(x) == 2:
+        return dist(a[0][0], a[0][1], a[1][0], a[1][1])
+    m = len(a) // 2 
+
+    return math.pi
+    
+    # divide array by 2
+    # base case, when array == 2
+    # return min val
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -12,4 +25,5 @@ if __name__ == '__main__':
     n = data[0]
     x = data[1::2]
     y = data[2::2]
-    print("{0:.9f}".format(minimum_distance(x, y)))
+    a = list(zip(x,y))
+    print("{0:.9f}".format(minimum_distance(a)))
