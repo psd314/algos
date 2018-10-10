3d741ff5212f4defb18493260bd4cc94a9f02a68
Philippe Dixon <phil.dixon@comparex.com>
2018-10-10 09:20:45 -0400

    starting change_dp

--- algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,8 +2,18 @@
 import sys
 
 def get_change(m):
+    coins = [1, 3, 4]
+    min_coins = [0] * (m+1)
+    for i in range(1, m+1):
+        min_coins[-1] = m
+        for j in range(len(coins)):
+            if i >= coins[j]:
+                num_coins = min_coins[i - coins[j]] + 1
+                if num_coins < min_coins[i]:
+                    min_coins[i] = num_coins
+
     #write your code here
-    return m // 4
+    return min_coins[-1]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
