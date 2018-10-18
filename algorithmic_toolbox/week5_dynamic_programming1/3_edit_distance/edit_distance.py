1deb27417684fd1ad160b02a6ea3840a05252620
Philippe Dixon <pdixon555@hotmail.com>
2018-10-18 06:29:18 -0400

    edit_distance passing test cases

--- algorithmic_toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ algorithmic_toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -4,19 +4,25 @@ def edit_distance(s, t):
     d = [ [i]+list(range(1, len(t)+1)) if i==0 else [i] + [0]*len(t) for i in range(len(s)+1) ] 
     m = len(s)
     n = len(t)
-    #for i in range(1, m):
-    #    for j in range(1, n):
-    #        ins = d[i][j-1] + 1
-    #        delete = d[i-1][j] + 1
-    #        match = d[i-1][j-1] + 1
-    #        mismatch = d[i-1][j-1] + 1
-    #        if s[i] == t[j]:
-    #            d[i][j] = min(ins, delete, match)
-    #        else:
-    #            d[i][j] = min(ins, delete, mismatch)
     
-    print('d', d, 'm', m, 'n', n)
-    return d[m-1][n-1]
+    for i in range(1, m+1):
+       for j in range(1, n+1):
+        #    print('i', i, 'j', j)
+        #    print('s', s[i-1], 't', t[j-1])
+        #    print()
+           ins = d[i][j-1] + 1
+           delete = d[i-1][j] + 1
+           match = d[i-1][j-1] 
+           mismatch = d[i-1][j-1] + 1
+           if s[i-1] == t[j-1]:
+               d[i][j] = min(ins, delete, match)
+           else:
+               d[i][j] = min(ins, delete, mismatch)
+    
+    # for k in d:
+    #     print(k)
+    # print('m', m, 'n', n)
+    return d[m][n]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
