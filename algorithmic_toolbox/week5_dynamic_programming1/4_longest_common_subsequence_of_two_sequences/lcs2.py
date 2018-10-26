587162c87ff983767892da8fc19c7b2bd761bf6b
Philippe Dixon <phil.dixon@comparex.com>
2018-10-26 08:01:48 -0400

    edit distance completed

--- algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -4,7 +4,26 @@ import sys
 
 def lcs2(a, b):
     #write your code here
-    return min(len(a), len(b))
+    m = len(a)
+    n = len(b)
+    dp = [ [0]*(len(b)+1) for i in range(len(a)+1) ] 
+    print('m:', m, 'n:', n)
+    for j in range(1, m+1):
+        for k in range(1, n+1):
+            print('j:', j, 'k:', k)
+            print('a[j-1]', a[j-1], 'b[k-1]', b[k-1])
+            if a[j-1] == b[k-1]:
+                dp[j][k] = dp[j-1][k]+1
+            else:
+                dp[j][k] = dp[j][k-1]
+                print(False, dp[j][k-1])
+                for i in dp:
+                    print(i)
+            print('dp[j][k]', dp[j][k])
+            print()
+    for l in dp:
+        print(l)
+    return dp
 
 if __name__ == '__main__':
     input = sys.stdin.read()
