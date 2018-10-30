e3fd6704283e0942189f9c79d54cb3628d5a8598
Dixon <phil.dixon@comparex.com>
2018-10-30 09:27:37 -0400

    knapsack.py passing test case

--- algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -3,12 +3,24 @@ import sys
 
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
-
+    w = [0] + sorted(w, reverse=True)
+    #print(w)
+    value = [ [0]*(W+1) for i in range(len(w)) ]
+    for i in range(1, len(w)):
+        for j in range(1, W+1):
+            #print('i,j', i, j)
+            value[i][j] = value[i-1][j]      
+            if w[i] <= j:
+                val = value[i-1][j-w[i]] + w[i]
+                if value[i][j] < val:
+                    value[i][j] = val
+    count = 0
+    #for k in value:
+    #    if count == 0:
+    #        print(' ',list(range(len(k))))
+    #    print(count, k)
+    #    count += 1
+    return value[n][W]
 if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
